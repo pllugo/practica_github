c7862afc33f5d0b10050e033f1a2e20f7e80f7fa
Pedro Luis Lugo Garcia <72478141+pllugo@users.noreply.github.com>
2022-05-17 19:48:49 -0300

    Segundo Commit

--- src/mascotasgithub/Entidades/Perro.java
+++ src/mascotasgithub/Entidades/Perro.java
@@ -13,4 +13,56 @@ public class Perro {
     private String nombre;
     private Integer edad;
     private String apodo;
+    private String color;
+
+    public Perro() {
+    }
+
+    public Perro(String nombre, Integer edad, String apodo, String color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apodo = apodo;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "nombre=" + nombre + ", edad=" + edad + ", apodo=" + apodo + ", color=" + color + '}';
+    }
+
+    
+    
+    
 }
